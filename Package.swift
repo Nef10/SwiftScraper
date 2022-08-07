b3c27ad7adbbf5fbed2ad05fc4cffaaae89c284e
Steffen Kötte <steffen.koette@gmail.com>
2022-08-07 15:10:04 -0700

    Use logger

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@ import PackageDescription
 
 let package = Package(
     name: "SwiftScraper",
-    platforms: [.iOS(.v9), .macOS(.v10_11)],
+    platforms: [.iOS(.v14), .macOS(.v11)],
     products: [
         .library(
             name: "SwiftScraper",
